303bb3d159ccb28aab9429ade0a15f79898c218d
Kuria-chan18 <113060036+Kuria-chan18@users.noreply.github.com>
2023-10-01 16:32:11 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import Link from './Components/Link'
 import LogoTipo from './Components/LogoTipo'
 import Menu from './Components/Menu'
-import NavBar from './Components/NavBar';
+import NavBar from './Components/NavBar'
 import NavMenuMobile from './Components/NavMenuMobile'
 
 function App() {
